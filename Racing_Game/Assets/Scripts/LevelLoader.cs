efa7cd88fd08f275e1c6eca03ba4d0be54f85a04
Excalibur <wdm.tushar@gmail.com>
2018-06-29 10:32:38 +0530

    new update
    new update

---
+++ Racing_Game/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+	public GameObject MenuCanvas , SettingCanvas ;
+	void Start () {
+		MenuCanvas.SetActive(true);
+		SettingCanvas.SetActive(false);
+	}
+	public void LoadLevel(int a){
+		SceneManager.LoadScene(a);
+	}
+	public void Quit(){
+		Application.Quit();
+	}
+	public void Setting(){
+		MenuCanvas.SetActive(false);
+		SettingCanvas.SetActive(true);
+	}
+	public void Settings_backButton(){
+		MenuCanvas.SetActive(true);
+		SettingCanvas.SetActive(false);
+	}
+}
